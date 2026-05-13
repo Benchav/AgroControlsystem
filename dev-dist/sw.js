b179d0ac323db7ac3d3710176534b3d854d5bb8f
benchav <joshua44benja@gmail.com>
2026-05-12 19:15:32 -0600

    update revision number for precached index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tokho4n3168"
+    "revision": "0.b6f88ftla08"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
