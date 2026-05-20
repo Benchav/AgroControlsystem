91e97721af41ff6af51665516e5edb723e946126
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-05-19 19:46:25 -0600

    feat: add images in dashboard with charts

--- src/entities/dashboard_metric.ts
+++ src/entities/dashboard_metric.ts
@@ -1,6 +1,10 @@
+import { color } from "./../../node_modules/@types/d3-color/index.d";
 export type DashboardMetric = {
   label: string;
   value: string;
+  description?: string;
+  color: string;
+  chartData: { value: number }[];
   icon: string;
 };
 
